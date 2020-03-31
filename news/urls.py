ba235246943cdd45d30f431a91796d2b7284aae5
Pomidor-GG <mishpzr@yandex.ru>
2020-03-31 12:29:15 +0300

    init

---
+++ news/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', landing, name='landing_url'),
+    path('news/', news_list, name='news_list_url'),
+    path('news/<str:slug>/', news_detail, name='news_detail_url'),
+    path('reviews/', reviews_list, name='reviews_list_url'),
+    path('reviews/<str:slug>/', review_detail, name='review_detail_url'),
+    path('not_working/', not_working, name='not_working_url'),
+    path('ikiwinner/', ikiwinner, name="ikiwinner_url"),
+    path('callback/', callback_view, name="callback_view_url"),
+
+]
---
+++ news/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from .models import *
+
+from .API import api_slider
+
+
+def landing(request):
+    posts = Post.objects.all()
+    reviews = Review.objects.all()
+    previews = api_slider()
+    return render(request, 'news/landing.html', context={'posts': posts, 'reviews': reviews, 'previews': previews})
+
+
+def news_list(request):
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+    return render(request, 'news/news_list.html', context={'posts': posts})
+
+
+def news_detail(request, slug):
+    post = get_object_or_404(Post, slug__iexact=slug)
+    return render(request, 'news/news_detail.html', context={'post': post})
+
+
+def reviews_list(request):
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        reviews = Review.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        reviews = Review.objects.all()
+    return render(request, 'reviews/reviews_list.html', context={'reviews': reviews})
+
+
+def review_detail(request, slug):
+    review = get_object_or_404(Review, slug__iexact=slug)
+    return render(request, 'reviews/review_detail.html', context={'review': review})
+
+
+def not_working(request):
+    return render(request, 'working/not_working.html')
+
+
+def ikiwinner(request):
+    return render(request, 'working/ikiwinner.html')
+
+
+def callback_view(request):
+  data = request.POST
